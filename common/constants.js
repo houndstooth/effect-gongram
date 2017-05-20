9a94b6f726ff88eb98d98593c182c4e8cd43d7fe
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-05-20 16:09:38 +0900

    overhaul local and shared customization and grid options arguments from file variables into animatable centralized state object; also centralize animation

--- common/constants.js
+++ common/constants.js
@@ -1,10 +1,11 @@
 import iterator from '../../shared/utilities/iterator'
-import { GRID_SIZE } from '../../shared/common/customize'
+import state from '../../state'
 import { BLACK, BLUE, WHITE, GREEN, RED } from '../../shared/common/colors'
 
 const GONGRAM_COLORS = [ BLACK, BLUE, WHITE, GREEN, RED ]
 
-const GONGRAM_SUPERTILE = iterator(GRID_SIZE).map(x => iterator(GRID_SIZE).map(y => {
+const gridSize = state.shared.gridSize
+const GONGRAM_SUPERTILE = iterator(gridSize).map(x => iterator(gridSize).map(y => {
 	return [
 		GONGRAM_COLORS[ (y * 2) % 5 ],
 		GONGRAM_COLORS[ x % 5 ]
