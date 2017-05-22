92dc4348b13d8c043c2720c75e4079b1b664f714
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-22 18:00:58 +0900

    fix bug ... uh yeah
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- gongramConstants.js
+++ gongramConstants.js
@@ -1,11 +1,9 @@
 import iterator from '../shared/utilities/iterator'
-import state from '../state'
 import { BLACK, BLUE, WHITE, GREEN, RED } from '../shared/render/colors'
 
 const GONGRAM_COLORS = [ BLACK, BLUE, WHITE, GREEN, RED ]
 
-const gridSize = state.shared.gridSize
-const GONGRAM_SUPERTILE = iterator(gridSize).map(x => iterator(gridSize).map(y => {
+const GONGRAM_SUPERTILE = iterator(5).map(x => iterator(5).map(y => {
     return [
         GONGRAM_COLORS[ (y * 2) % 5 ],
         GONGRAM_COLORS[ x % 5 ]
