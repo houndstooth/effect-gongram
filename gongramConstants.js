10ff52a2dbb89c936726529162c394b676766b44
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-23 17:43:01 +0900

    refactor application code
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- gongramConstants.js
+++ gongramConstants.js
@@ -1,15 +1,15 @@
 import iterator from '../shared/utilities/iterator'
-import { BLACK, BLUE, WHITE, GREEN, RED } from '../shared/render/colors'
+import { BLACK, BLUE, GREEN, RED, WHITE } from '../shared/render/colors'
 
 const GONGRAM_COLORS = [ BLACK, BLUE, WHITE, GREEN, RED ]
 
 const GONGRAM_SUPERTILE = iterator(5).map(x => iterator(5).map(y => {
-    return [
-        GONGRAM_COLORS[ (y * 2) % 5 ],
-        GONGRAM_COLORS[ x % 5 ]
-    ]
+	return [
+		GONGRAM_COLORS[ (y * 2) % 5 ],
+		GONGRAM_COLORS[ x % 5 ]
+	]
 }))
 
 export {
-    GONGRAM_SUPERTILE
+	GONGRAM_SUPERTILE
 }
