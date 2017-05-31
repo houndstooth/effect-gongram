c4f9cd07fa4a3d95a7f31c670fe332e79f706ae1
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-05-31 23:05:53 +0900

    gongram picks some houndazzle colors too

--- gongramPreset.js
+++ gongramPreset.js
@@ -12,6 +12,18 @@ export default {
 						rows: iterator(5).map(y => (y * 2) % 5),
 						columns: iterator(5)
 					}
+				},
+				houndazzle: {
+					color: {
+						set: [ BLACK, BLUE, WHITE, GREEN, RED ],
+						assignment: {
+							supertile: iterator(5).map(x => iterator(5).map(y => [ (y * 3 + 1) % 5, (x + 4) % 5 ])),
+							weave: {
+								rows: iterator(5).map(y => (y * 3 + 1) % 5),
+								columns: iterator(5).map(x => (x + 4) % 5)
+							}
+						}
+					}
 				}
 			}
 		}
