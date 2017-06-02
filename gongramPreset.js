69be480e0afe6d14933174177f13b1f2656c4f00
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-02 12:15:30 +0900

    plays nice with dazzle
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- gongramPreset.js
+++ gongramPreset.js
@@ -12,6 +12,20 @@ export default {
 						rows: iterator(5).map(y => (y * 2) % 5),
 						columns: iterator(5)
 					}
+				},
+				houndazzle: {
+					colorConfig: {
+						assignment: {
+							mode: 'SUPERTILE',
+							supertile: iterator(5).map(x => iterator(5).map(y => [ x , (y * 2) % 5 ]))
+						}
+					},
+					orientationConfig: {
+						assignment: {
+							mode: 'WEAVE',
+							weave: { rows: [ 1, 0 ], columns: [ 0, 1 ] }
+						}
+					}
 				}
 			}
 		}
