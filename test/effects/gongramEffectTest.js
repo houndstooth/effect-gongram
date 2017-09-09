d4699325f9926e41d8a5273d79372e80527cfcba
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-09-09 19:54:13 +0900

    break off parts of store and controls into execute

--- test/effects/gongramEffectTest.js
+++ test/effects/gongramEffectTest.js
@@ -1,4 +1,4 @@
-import executeSelectedHoundstoothEffects from '../../../../src/controls/executeSelectedHoundstoothEffects'
+import executeSelectedHoundstoothEffects from '../../../../src/execute/executeSelectedHoundstoothEffects'
 import standardTileIsColors from '../../../../test/integration/helpers/standardTileIsColors'
 import activateTestMarkerCanvas from '../../../../test/integration/helpers/activateTestMarkerCanvas'
 import { BLACK, BLUE, WHITE, GREEN, RED } from '../../../../src/constants'
