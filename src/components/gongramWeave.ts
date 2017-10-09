4f0edc34aa9b72adfbb23df77a303e5e35467f7d
Douglas <douglas.blumeyer@gmail.com>
2017-10-09 18:03:41 +0800

    new eslint rule to bring toward conformity with tslint recommendations - no unnecessary parens

--- src/components/gongramWeave.ts
+++ src/components/gongramWeave.ts
@@ -1,7 +1,7 @@
 import { iterator } from '../../../../src/utilities/codeUtilities'
 
 const gongramWeave = () => ({
-	rows: iterator(5).map(y => (y * 2) % 5),
+	rows: iterator(5).map(y => y * 2 % 5),
 	columns: iterator(5),
 })
 
