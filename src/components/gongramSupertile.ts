5ba7cdf85a5600ecffec63438157a296685de898
Douglas <douglas.blumeyer@gmail.com>
2017-10-15 09:59:26 +0800

    tslint all

--- src/components/gongramSupertile.ts
+++ src/components/gongramSupertile.ts
@@ -1,12 +1,9 @@
 import { iterator } from '../../../../src/utilities/codeUtilities'
 import { Supertile } from '../../../../src'
 
-const gongramSupertile: { (): Supertile } = () => {
-	return iterator(5).map(x => {
-		return iterator(5).map(y => {
-			return [ y * 2 % 5, x ]
-		})
-	}) as Supertile
-}
+const gongramSupertile: { (): Supertile } = () =>
+	iterator(5).map(x =>
+		iterator(5).map(y =>
+			[ y * 2 % 5, x ])) as Supertile
 
 export default gongramSupertile
