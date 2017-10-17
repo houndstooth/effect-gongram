76cdd8859db7c3077b213be402a40950458cae50
Douglas <douglas.blumeyer@gmail.com>
2017-10-18 03:05:06 +0800

    callable types, after figuring out the syntax to use with destructured object params

--- src/components/gongramSupertile.ts
+++ src/components/gongramSupertile.ts
@@ -1,7 +1,7 @@
 import { Supertile } from '../../../../src'
 import { iterator } from '../../../../src/utilities/codeUtilities'
 
-const gongramSupertile: { (): Supertile } = () =>
+const gongramSupertile: () => Supertile = () =>
 	iterator(5).map(x =>
 		iterator(5).map(y =>
 			[ y * 2 % 5, x ])) as Supertile
--- src/components/gongramWeave.ts
+++ src/components/gongramWeave.ts
@@ -1,7 +1,7 @@
 import { Weave } from '../../../../src'
 import { iterator } from '../../../../src/utilities/codeUtilities'
 
-const gongramWeave: { (): Weave } = () => ({
+const gongramWeave: () => Weave = () => ({
 	columns: iterator(5),
 	rows: iterator(5).map(y => y * 2 % 5),
 })
