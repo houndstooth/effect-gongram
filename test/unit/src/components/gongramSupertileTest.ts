a5554ebdc0742a9b5aae6cb7c45f7bc1622ebeb0
Douglas <douglas.blumeyer@gmail.com>
2017-10-23 13:14:35 +0800

    use grid for double arrays

--- test/unit/src/components/gongramSupertileTest.ts
+++ test/unit/src/components/gongramSupertileTest.ts
@@ -1,16 +1,17 @@
+import { to } from '../../../../../../src'
 import { gongramSupertile } from '../../../../src/components/gongramSupertile'
 
 describe('gongram supertile', () => {
 	it('makes a rank-2 array of addresses for 5 colors, one dimension by 2s and the other by 1s', () => {
 		const actualSupertile = gongramSupertile()
 
-		const expectedSupertile = [
+		const expectedSupertile = to.Supertile([
 			[ [ 0, 0 ], [ 2, 0 ], [ 4, 0 ], [ 1, 0 ], [ 3, 0 ] ],
 			[ [ 0, 1 ], [ 2, 1 ], [ 4, 1 ], [ 1, 1 ], [ 3, 1 ] ],
 			[ [ 0, 2 ], [ 2, 2 ], [ 4, 2 ], [ 1, 2 ], [ 3, 2 ] ],
 			[ [ 0, 3 ], [ 2, 3 ], [ 4, 3 ], [ 1, 3 ], [ 3, 3 ] ],
 			[ [ 0, 4 ], [ 2, 4 ], [ 4, 4 ], [ 1, 4 ], [ 3, 4 ] ],
-		]
+		])
 
 		expect(actualSupertile).toEqual(expectedSupertile)
 	})
